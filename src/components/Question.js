a757e51e54fba913c49f990c66e0636ba9e5f6ec
generalentropy <entropie@tuta.io>
2023-12-26 10:33:51 +0100

    questions

---
+++ src/components/Question.js
@@ -0,0 +1,12 @@
+import Options from "./Options";
+
+function Question({ question }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question}></Options>
+    </div>
+  );
+}
+
+export default Question;
